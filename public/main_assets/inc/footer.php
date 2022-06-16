50f9e5ebba7aebf55f17982abb80fb9f3e9e190f
ddeveloperuzair <55616568+ddeveloperuzair@users.noreply.github.com>
2022-06-16 14:41:31 -0700

    Add files via upload

---
+++ public/main_assets/inc/footer.php
@@ -0,0 +1,74 @@
+ <!-- Footer -->
+           <footer id="footer" class="footer-light-style clearfix">
+            <div class="themesflat-container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-12 col-12">
+                        <div class="widget widget-logo">
+                            <div class="logo-footer" id="logo-footer">
+                                <a href="javascript:;">
+                                    <img id="logo_footer" src="assets/images/logo/logo_dark.png" alt="nft-gaming" width="135" height="56"
+                                    data-retina="assets/images/logo/logo_dark@2x.png" data-width="135"
+                                    data-height="56">
+                                </a>
+                            </div>
+                            <p class="sub-widget-logo">Lorem ipsum dolor sit amet,consectetur adipisicing elit. Quis non, fugit totam vel laboriosam vitae.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-sm-5 col-5">
+                        <div class="widget widget-menu style-1">
+                            <h5 class="title-widget">My Account</h5>
+                            <ul>
+                                <li><a href="javascript:;">Authors</a></li>
+                                <li><a href="javascript:;">Collection</a></li>
+                                <li><a href="javascript:;">Author Profile</a></li>
+                                <li><a href="javascript:;">Create Item</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-sm-7 col-7">
+                        <div class="widget widget-menu style-2">
+                            <h5 class="title-widget">Resources</h5>
+                            <ul>
+                                <li><a href="javascript:;">Help & Support</a></li>
+                                <li><a href="javascript:;">Live Auctions</a></li>
+                                <li><a href="javascript:;">Item Details</a></li>
+                                <li><a href="javascript:;">Activity</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4 col-sm-5 col-5">
+                        <div class="widget widget-menu fl-st-3">
+                            <h5 class="title-widget">Company</h5>
+                            <ul>
+                                <li><a href="javascript:;">Explore</a></li>
+                                <li><a href="javascript:;">Contact Us</a></li>
+                                <li><a href="javascript:;">Our Blog</a></li>
+                                <li><a href="javascript:;">FAQ</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 col-sm-7 col-12">
+                        <div class="widget widget-subcribe">
+                            <h5 class="title-widget">Subscribe Us</h5>
+                            <div class="form-subcribe">
+                                <form id="subscribe-form" action="#" method="GET" accept-charset="utf-8" class="form-submit">
+                                    <input name="email" value="" class="email" type="email" placeholder="info@yourgmail.com" required>
+                                    <button id="submit" name="submit" type="submit"><i class="icon-fl-send"></i></button>
+                                </form>
+                            </div>
+                            <div class="widget-social style-1 mg-t32">
+                                <ul>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+                                    
+                                    <li class="style-2"><a href="#"><i class="fab fa-telegram-plane"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                    <li class="mgr-none"><a href="#"><i class="icon-fl-tik-tok-2"></i></a></li>
+                                    <li class="mgr-none"><a href="#"><i class="icon-fl-vt"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </footer><!-- /#footer -->
---
+++ public/main_assets/inc/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>NFT WINDOWS | NFT Marketplace</title>
+
+    <meta name="author" content="themesflat.com">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <!-- Favicon and Touch Icons  -->
+    <link rel="shortcut icon" href="assets/icon/Favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/icon/Favicon.png">
+
+</head>
